7dad0585d954adba8d4c083cb85274267c7bd603
Alex Ciceu <alex@alexciceu.com>
2023-04-28 11:17:34 +0300

    Timezone fix (#2)

--- cogs/formulaone.py
+++ cogs/formulaone.py
@@ -1,9 +1,10 @@
-import time
 import fastf1
+import pytz
 from disnake import Embed, ApplicationCommandInteraction
 from disnake.ext import commands
 from config import Configuration
-
+from datetime import datetime   
+import calendar
 
 class FormulaOne(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -20,26 +21,25 @@ class FormulaOne(commands.Cog):
         for x in range(0, 30):
             try:
                 event = sessions.get_event_by_round(x)
-
                 embed = Embed(
                     title=event['EventName'],
                     type='rich',
                     color=0xe3088b
                 )
                 embed.add_field(name=event['Session1'],
-                                value=convert_discord_localized_timestamp(event['Session1Date']),
+                                value=convert_discord_localized_timestamp(event['Session1Date'], x),
                                 inline=False)
                 embed.add_field(name=event['Session2'],
-                                value=convert_discord_localized_timestamp(event['Session2Date']),
+                                value=convert_discord_localized_timestamp(event['Session2Date'], x),
                                 inline=False)
                 embed.add_field(name=event['Session3'],
-                                value=convert_discord_localized_timestamp(event['Session3Date']),
+                                value=convert_discord_localized_timestamp(event['Session3Date'], x),
                                 inline=False)
                 embed.add_field(name=event['Session4'],
-                                value=convert_discord_localized_timestamp(event['Session4Date']),
+                                value=convert_discord_localized_timestamp(event['Session4Date'], x),
                                 inline=False)
                 embed.add_field(name=event['Session5'],
-                                value=convert_discord_localized_timestamp(event['Session5Date']),
+                                value=convert_discord_localized_timestamp(event['Session5Date'], x),
                                 inline=False)
                 
                 await interaction.response.send_message(embed=embed)
@@ -50,8 +50,16 @@ class FormulaOne(commands.Cog):
                 break
 
 
-def convert_discord_localized_timestamp(dt):
-    return f"<t:{round(time.mktime(time.strptime(str(dt), '%Y-%m-%d %H:%M:%S')))}:f>"
+def convert_discord_localized_timestamp(dt, tzi):
+    tz = ["Asia/Bahrain", "Asia/Bahrain", "Asia/Riyadh", "Australia/Melbourne", "Asia/Baku", "America/Detroit", "Europe/Rome", "Europe/Monaco", "Europe/Madrid", "America/Montreal", "Europe/Vienna", "Europe/London", "Europe/Budapest", "Europe/Brussels", "Europe/Amsterdam", "Europe/Rome", "Asia/Singapore", "Asia/Tokyo", "Asia/Qatar", "America/Chicago", "America/Mexico_City", "America/Sao_Paulo", "America/Los_Angeles", "Asia/Baku"] 
+
+    local = pytz.timezone(tz[tzi])
+    naive = datetime.strptime(str(dt), '%Y-%m-%d %H:%M:%S')
+    local_dt = local.localize(naive)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    utc_timestamp = round(calendar.timegm(utc_dt.timetuple()))
+
+    return f"<t:{utc_timestamp}:f>"
 
 
 def setup(bot: commands.Bot):
