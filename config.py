cf2e9a1fe9c841d56eed60e946e54bf4e98c2bc1
Yuri Hoogeweg <yurihoogeweg@hotmail.com>
2025-03-09 20:37:04 +0000

    oops

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Configuration:
 
             # Read config variables from config.ini
             config = ConfigParser()
-            config.read('devconfig.ini')
+            config.read('config.ini')
             cls._instance.GUILD_ID = int(config['discord']['GUILD_ID'])
             cls._instance.CLIENT_ID = int(config['discord']['CLIENT_ID'])
             cls._instance.TOKEN = config['discord']['TOKEN']
